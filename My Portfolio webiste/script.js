4d9e1b8d88d2058a95f69f7832f252321aa02b98
kezhongithub <152598161+kezhongithub@users.noreply.github.com>
2023-12-01 00:36:28 -0800

    Add files via upload

---
+++ My Portfolio webiste/script.js
@@ -0,0 +1,6 @@
+let type = new Typed("#changing-text", {
+    strings: ["BSIT-1 Student", "Kezh", "Bachelor of Science Technology"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
---
+++ script.js
@@ -0,0 +1,6 @@
+let type = new Typed("#changing-text", {
+    strings: ["BSIT-1 Student", "Bibliophile", "Geek/Anorak"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
